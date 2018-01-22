311c5a4318173a94627e1e0ad3b24d9a9c8c4c9b
Oli Passey <oli@hedgerows.net>
2018-01-22 20:03:59 +0000

    Create autoexec.py

---
+++ autoexec.py
@@ -0,0 +1,7 @@
+# ======================================================================== #
+# place this file in the userdata folder of a LibreElec rPi                #
+# once Kodi has loaded it will display the awaiting_data image             #
+# until KodiKontroller picks it up as online and sends a content command   #
+# ======================================================================== #
+import xbmc
+xbmc.executebuiltin("SlideShow(/storage/pictures/awaiting_data.jpg)")
